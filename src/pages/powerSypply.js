e56110828d017d7c9d291af1d16181747e2ba278
saymon <mdsaymonshoab@gmail.com>
2023-07-31 07:54:56 +0600

    all catagory page done

--- src/pages/powerSypply.js
+++ src/pages/powerSypply.js
@@ -4,10 +4,76 @@ import { Avatar, Card } from "antd";
 import Link from "next/link";
 const { Meta } = Card;
 const powerSypply = ({ allPcProduct }) => {
+  const filterCatagory = "Power Supply";
+  const filterdByCatagory = allPcProduct.filter(
+    (pcd) => pcd.catagory === filterCatagory
+  );
   return (
-    <div>
-      <h1>all power supplu hear</h1>
-    </div>
+    <>
+      <div className="flex justify-center items-center mt-10">
+        <h1 className="text-4xl font-bold text-center text-gray-600">
+          Storage Section
+        </h1>
+      </div>
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "50px",
+          marginBottom: "100px",
+        }}
+      >
+        {filterdByCatagory?.map((pcp, index) => (
+          <Col className="gutter-row" span={8}>
+            <Link href={`/product/${pcp?._id}`}>
+              <Card
+                className=" mx-24 my-10"
+                style={{
+                  width: 300,
+                }}
+                cover={
+                  <img
+                    alt="example"
+                    // src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    src={pcp?.image}
+                  />
+                }
+              >
+                <Meta
+                  avatar={
+                    <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+                  }
+                  title={pcp?.productName}
+                />
+                <span>
+                  <p className=" mt-3 font-bold">Price: {pcp?.price}</p>
+                </span>
+                <div className=" mt-3 flex justify-between">
+                  <Meta
+                    title={pcp?.catagory}
+                    // description={pcp?.price}
+                  />{" "}
+                  <Meta
+                    title={pcp?.status}
+                    // description={pcp?.price}
+                  />
+                  <Meta
+                    title={pcp?.avarageRating}
+                    // description={pcp?.price}
+                  />
+                </div>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
